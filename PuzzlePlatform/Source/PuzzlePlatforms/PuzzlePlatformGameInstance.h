68d07b9dce34631f7f6aab7c2863c98b1d846e4b
Advit Soni <zkc08@outlook.com>
2020-09-02 16:30:20 +0530

    Commit

---
+++ PuzzlePlatform/Source/PuzzlePlatforms/PuzzlePlatformGameInstance.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "PuzzlePlatformGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORMS_API UPuzzlePlatformGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	UPuzzlePlatformGameInstance(const FObjectInitializer & ObjectInitializer);
+
+	virtual void Init();
+
+	UFUNCTION(Exec)
+	void Host();
+
+	UFUNCTION(Exec)
+	void Join(const FString& Address);
+
+private:
+	TSubclassOf<class UUserWidget> MenuClass;
+	
+};
